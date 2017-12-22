f5c6e0749e3c07f89c4118d774db7f5e6c3a699b
codeconcept <contact@codeconcept.fr>
2017-12-22 15:58:30 +0100

    refactoring using switchMap operator to prevent nested subscribe() calls

--- src/app/estimate/estimate-details/estimate-details.component.ts
+++ src/app/estimate/estimate-details/estimate-details.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { EstimateService } from '../estimate.service';
 import { ActivatedRoute } from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+import { EstimateService } from '../estimate.service';
 
 @Component({
   selector: 'app-estimate-details',
@@ -10,17 +15,20 @@ import { ActivatedRoute } from '@angular/router';
 export class EstimateDetailsComponent implements OnInit {
   estimate;
 
-  constructor(private estimateService : EstimateService, private activatedRoute: ActivatedRoute) { }
+  constructor(private estimateService: EstimateService, private activatedRoute: ActivatedRoute) { }
 
 
   ngOnInit() {
-    const id = this.activatedRoute.paramMap.subscribe(params => {
-      const id = params.get('id');
-      console.log(id);
-      this.estimateService.getEstimateById(id).subscribe(estimate => {
+    const paramMap$ = this.activatedRoute.paramMap;
+
+    paramMap$
+      .map(paramMap => paramMap.get('id'))
+      .do(data => console.log('before switchMap', data))
+      .switchMap(id => this.estimateService.getEstimateById(id))
+      .do(data => console.log('after switchMap', data))
+      .subscribe(estimate => {
         this.estimate = estimate;
-      });
-    })
+      })
   }
 
 
